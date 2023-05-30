41940882143be39b892da1e0cee884889e3be97e
Aleksey Rodionov <rodion_obn@mail.ru>
2023-05-30 23:54:51 +0300

    Из ДЗ к семинару 3 решены задачи 21 и 23

---
+++ homework3/task21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Length(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    double result = Math.Sqrt((xb-xa)*(xb-xa) + (yb-ya)*(yb-ya) + (zb-za)*(zb-za));
+    return result;
+}
+
+System.Console.WriteLine("Введите первую координату точки А");
+double xa = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите вторую координату А");
+double ya = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите третью координату точки A");
+double za = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите первую координату точки B");
+double xb = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите вторую координату точки B");
+double yb = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите третью координату точки B");
+double zb = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine($"Расстояние между точками A и B равно: {Length(xa, ya, za, xb, yb, zb)}");
+
+
+
+
---
+++ homework3/task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+string ThirdDegree (int N)
+{
+    int count = 1;
+    int cube;
+    string result = "";
+
+    while (count <= N)
+    {
+        cube = count*count*count;
+        result = result + " " + cube.ToString();
+        count++;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите какое-нибудь целое положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"Таблица кубов числа {N}: {ThirdDegree (N)}");
+
+
+
+
+
