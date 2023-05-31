239f4d04d0074a9152821977f039a9b0845402f5
Aleksey Rodionov <rodion_obn@mail.ru>
2023-06-01 00:13:18 +0300

    Решена задача 19 из ДЗ к семинару 3

--- homework3/task19/Program.cs
+++ homework3/task19/Program.cs
@@ -3,4 +3,32 @@
 // 12821 -> да
 // 23432 -> да
 
+    string Polindrom(int number)
+    {
+    int tmp_number = number;
+    int reverse = 0;
+    string result = "";    
+    while(tmp_number!=0)
+        {
+        reverse = (reverse*10) + tmp_number%10;
+        tmp_number/=10;
+        }
+        Console.WriteLine($"Обратное число равно {reverse}");
+    if(number == reverse)
+        {
+        result="Введенное число является полиндромом";
+        }
+    else
+        result="Введенное число не является полиндромом";
+return result;
+    }
+
+    System.Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(Polindrom(number));
+    
+    
+    
+    
+    
 
