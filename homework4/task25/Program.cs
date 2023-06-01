ded10cf12d06936c328b78719c1843341d5a359f
Aleksey Rodionov <rodion_obn@mail.ru>
2023-06-01 23:30:31 +0300

    Решено ДЗ к семинару 4

---
+++ homework4/task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int exponentiation (int NumA, int NumB)
+{
+int result = NumA;
+for (int i = 1; i < NumB; i++)
+{
+    result = result*NumA;
+}
+
+return result;
+}
+
+System.Console.WriteLine("Введите число A");
+int NumA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B");
+int NumB = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(exponentiation (NumA, NumB));
+
---
+++ homework4/task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void Array(int amount)
+{
+    int [] numbers = new int [amount];
+    Random rand = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rand.Next(100);
+        Console.Write(numbers[i] + " ");
+    }
+}
+
+System.Console.WriteLine("Введите количество элементов массива");
+int amount = Convert.ToInt32(Console.ReadLine());
+Array(amount);
