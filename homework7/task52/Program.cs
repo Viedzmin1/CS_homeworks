7d48a91969042a877a4a543ffb4d35d2c502cdfb
Aleksey Rodionov <rodion_obn@mail.ru>
2023-06-15 00:01:38 +0300

    некорректный вывод в задаче 52

---
+++ homework7/task52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = i+j;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    ");
+        System.Console.WriteLine();
+        }
+}
+
+int[] SummCols (int[,] array)
+{
+    // int summCols = new int[array.GetLength(0)];
+    int summcols = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+        if (j<array.GetLength(1))
+        {
+            summcols = summcols+array[i,j];
+            j++;
+            System.Console.Write($"{summcols}, ");
+        }
+        }
+     return new int[] {summcols};
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
+int[] summcols = SummCols (array);
+
