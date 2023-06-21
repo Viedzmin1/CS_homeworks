a3becc8ef74aff35994f06d3e4904f9ec33635c7
Aleksey Rodionov <rodion_obn@mail.ru>
2023-06-21 23:38:07 +0300

    Решена задача 64

---
+++ homework8/task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+
+string GetNumbersRec(int num)
+{
+    if (num == 0)  return "";
+    return num + " " + GetNumbersRec(num-1);    
+}
+
+Console.Clear();
+
+System.Console.WriteLine("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(GetNumbersRec(num));
---
+++ homework8/task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Func(int M, int N)
+{
+    if (N == M) return 0;
+    return Func(M, N-1) + N;
+}
+
+System.Console.WriteLine("Введите целое число M ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите целое число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Func(M, N));
